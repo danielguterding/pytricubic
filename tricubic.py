6b9f8d9cd7d6523242b43afd30187986798531b0
Daniel Guterding <daniel.guterding@gmail.com>
2013-09-01 17:03:15 +0200

    clarified example

--- tricubic.py
+++ tricubic.py
@@ -2,14 +2,14 @@ import numpy as np
 import tricubic
 
 n = 3
-xyz = np.zeros((n,n,n), dtype='float')
+f = np.zeros((n,n,n), dtype='float')
 
 for i in range(n):
   for j in range(n):
     for k in range(n):
-      xyz[i][j][k] = i+j+k
+      f[i][j][k] = i+j+k #some function f(x,y,z) is given on a cubic grid indexed by i,j,k
       
-ip = tricubic.tricubic(list(xyz), [n,n,n])
+ip = tricubic.tricubic(list(f), [n,n,n]) #initialize interpolator with input data on cubic grid
 for i in range(100000):
-  res = ip.ip(list(np.random.rand(3)))
+  res = ip.ip(list(np.random.rand(3)*(n-1))) #interpolate the function f at a random point in space
   #print res

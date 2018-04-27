ae6fecb4480e865a603a9e73be78beef347fb1ec
Daniel Guterding <daniel.guterding@gmail.com>
2018-04-27 11:40:43 +0200

    Adapted code for pybind11 instead of boost.

--- tricubic.hpp
+++ tricubic.hpp
@@ -1,37 +1,41 @@
 #include <string>
 #include <sstream>
 #include <Eigen/Dense>
-#include <boost/array.hpp>
-#include <boost/multi_array.hpp>
-#include <boost/python.hpp>
+#include <pybind11/pybind11.h>
 
-using namespace boost::python;
-typedef float fptype;
+namespace py = pybind11;
+
+typedef double fptype;
 
 #ifndef TRI_CUBIC_INTERPOLATOR_H
 #define TRI_CUBIC_INTERPOLATOR_H
 
 //This code is adapted from https://github.com/deepzot/likely
-class TriCubicInterpolator{
+class TriCubicInterpolator
+{
   // Performs tri-cubic interpolation within a 3D periodic grid.
   // Based on http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.89.7835
-  public:
-    TriCubicInterpolator(list data, list nkpoints);
-    fptype ip(list xyz);
-  private:
-    boost::multi_array<fptype,1> _data;
-    fptype _spacing;
-    int _n1, _n2, _n3;
-    int _i1, _i2, _i3;
-    bool _initialized;
-    Eigen::Matrix<fptype,64,1> _coefs;
-    Eigen::Matrix<fptype,64,64> _C;
-    inline int _index(int i1, int i2, int i3) const {
-        if((i1 %= _n1) < 0) i1 += _n1;
-        if((i2 %= _n2) < 0) i2 += _n2;
-        if((i3 %= _n3) < 0) i3 += _n3;
-        return i1 + _n1*(i2 + _n2*i3);
-	}
+public:
+  TriCubicInterpolator(py::list data, py::list nkpoints);
+  fptype ip(py::list xyz);
+private:
+  fptype *_data_ptr;
+  fptype _spacing;
+  int _n1, _n2, _n3;
+  int _i1, _i2, _i3;
+  bool _initialized;
+  Eigen::Matrix<fptype, 64, 1> _coefs;
+  Eigen::Matrix<fptype, 64, 64> _C;
+  inline int _index(int i1, int i2, int i3) const
+  {
+    if ((i1 %= _n1) < 0)
+      i1 += _n1;
+    if ((i2 %= _n2) < 0)
+      i2 += _n2;
+    if ((i3 %= _n3) < 0)
+      i3 += _n3;
+    return i1 + _n1 * (i2 + _n2 * i3);
+  }
 };
 
 #endif

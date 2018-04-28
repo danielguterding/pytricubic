c7c39dd0901aa6cb038d5837598d1348fcadab8d
Daniel Guterding <guterding@itp.uni-frankfurt.de>
2018-04-28 23:15:07 +0200

    Fixed example file.

--- example.py
+++ example.py
@@ -12,4 +12,4 @@ for i in range(n):
 ip = tricubic.tricubic(list(f), [n,n,n]) #initialize interpolator with input data on cubic grid
 for i in range(100000):
   res = ip.ip(list(np.random.rand(3)*(n-1))) #interpolate the function f at a random point in space
-  #print res
+  #print(res)

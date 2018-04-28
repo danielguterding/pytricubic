3709ffd08655e95d6e68bc99d5fb7c7d29b20957
Daniel Guterding <guterding@itp.uni-frankfurt.de>
2018-04-28 23:23:19 +0200

    Fixed setup script.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version = "1.0.0",
     url = "https://github.com/danielguterding/pytricubic",
     author = "Daniel Guterding",
+    author_email = "daniel.guterding@gmail.com",
     license = "MIT",
     keywords = "tricubic cubic interpolation",
     # Build instructions

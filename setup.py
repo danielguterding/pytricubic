d98f1ef655cd628cbb0f25f77b5370f55bc6c5e2
Daniel Guterding <daniel.guterding@gmail.com>
2025-11-19 11:37:22 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ class CatchTestCommand(TestCommand):
 setup(
     # Information
     name = "tricubic",
-    version = "1.0.4",
+    version = "1.0.5",
     url = "https://github.com/danielguterding/pytricubic",
     author = "Daniel Guterding",
     author_email = "daniel.guterding@gmail.com",
